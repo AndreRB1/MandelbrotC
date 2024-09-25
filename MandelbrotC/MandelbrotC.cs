eeede8309cee95743bcfd628bf209b14aa8c7155
janhuls <156079202+janhuls@users.noreply.github.com>
2024-09-25 14:57:38 +0200

    added button for color selecting

--- MandelbrotC/MandelbrotC.cs
+++ MandelbrotC/MandelbrotC.cs
@@ -14,42 +14,98 @@ Form scherm = new Form();
 scherm.Text = "MandelbrotC";
 scherm.BackColor = Color.White;
 scherm.ClientSize = new Size(600, 400);
-Label labx = new Label(); Label laby = new Label();Label labscale = new Label();
-TextBox center_x = new TextBox(); 
+
+//***LABELS***
+//label for x coord
+Label labx = new Label(); 
+scherm.Controls.Add(labx);
+labx.Location = new Point(6, 6); 
+labx.Size = new Size(30, 30); 
+labx.Text = "x:";
+//label for y coord
+Label laby = new Label();
+scherm.Controls.Add(laby);
+laby.Location = new Point(91, 6); 
+laby.Size = new Size(30, 20); 
+laby.Text = "y:";
+//label for scale
+Label labscale = new Label();
+scherm.Controls.Add(labscale);
+labscale.Location = new Point(6, 30); 
+labscale.Size = new Size(41, 20); 
+labscale.Text = "schaal:";
+
+//***TEXTBOXES***
+//textbox for x coord
+TextBox center_x = new TextBox();
+scherm.Controls.Add(center_x);
+center_x.Location = new Point(46, 6);
+center_x.Size = new Size(45, 20); 
+center_x.Text = "300";
+//textbox for y coord
 TextBox center_y = new TextBox();
+scherm.Controls.Add(center_y);
+center_y.Location = new Point(126, 6); 
+center_y.Size = new Size(45, 20); 
+center_y.Text = "200";
+//textbox for scale
 TextBox scale_in = new TextBox();
+scherm.Controls.Add(scale_in);
+scale_in.Location = new Point(46, 26); 
+scale_in.Size = new Size(80, 20); 
+scale_in.Text = "0.01";
+
+//***BUTTONS***
+//button for calculating with given input
 Button knop = new Button();
-scherm.Controls.Add(labx);scherm.Controls.Add(laby);scherm.Controls.Add(labscale);
-scherm.Controls.Add(center_x);scherm.Controls.Add(center_y);scherm.Controls.Add(scale_in);scherm.Controls.Add(knop);
-labx.Location = new Point(6, 6); labx.Size = new Size(30, 30); labx.Text = "x:";
-center_x.Location = new Point(46, 6);center_x.Size = new Size(45, 20); center_x.Text = "300";
-laby.Location = new Point(91, 6); laby.Size = new Size(30, 20); laby.Text = "y:";
-center_y.Location = new Point(126, 6);center_y.Size = new Size(45, 20); center_y.Text = "200";
-labscale.Location = new Point(6, 30); labscale.Size = new Size(41, 20); labscale.Text = "schaal:";
-scale_in.Location = new Point(46, 26); scale_in.Size = new Size(80, 20); scale_in.Text = "0.01";
-knop.Location = new Point(126, 26); knop.Size = new Size(80, 20); knop.Text = "bereken";
-
-
-//create color palatte
-Color zero = Color.Black;
-Color outer = Color.Yellow;
-List<Color> colors = new List<Color>();
-for (double i = 0; i<= n; i++)
+scherm.Controls.Add(knop);
+knop.Location = new Point(126, 26); 
+knop.Size = new Size(80, 20); 
+knop.Text = "bereken";
+//button for selecting the inner color
+Button innercolor = new Button();
+scherm.Controls.Add(innercolor);
+innercolor.Location = new Point(206, 6); innercolor.Size = new Size(80, 20); innercolor.Text = "kleur 1";
+//button for selecting outer color
+Button outercolor = new Button();
+scherm.Controls.Add(outercolor);
+outercolor.Location = new Point(206, 26); 
+outercolor.Size = new Size(80, 20); 
+outercolor.Text = "kleur 2";
+//generate a starting list of colors
+Color inrclr = Color.Black;
+Color outrclr = Color.White;
+List<Color> colors = gen_palette(inrclr, outrclr);
+List<Color> gen_palette(Color zero, Color outer)//create color palatte
 {
-    Color colori = Color.FromArgb((byte)(zero.R * (i / n) + outer.R * (1 - i / n)), (byte)(zero.G * (i / n) + outer.G * (1 - i / n)), (byte)(zero.B * (i / n) + outer.B * (1 - i / n)));
-    colors.Add(colori);
+    List<Color> colors = new List<Color>();
+    for (double i = 0; i <= n; i++)
+    {
+        // gradient from color zero to color outer
+        byte r = (byte)(zero.R * (i / n) + outer.R * (1 - i / n));
+        byte g = (byte)(zero.G * (i / n) + outer.G * (1 - i / n));
+        byte b = (byte)(zero.B * (i / n) + outer.B * (1 - i / n));
+        //*/
+        Color colori = Color.FromArgb(r, g, b);
+        colors.Add(colori);
+        
+    }
+    return colors;
 }
-
 Color in_set(double x,double y) //checkt voor elk input punt wat het mandel getal is, en output een kleur
 {
+    
     x = ((x- center.X) *scale); y= ((y-center.Y) *scale);
     double a = 0; double b = 0;
+    double a_sq = 0; double b_sq = 0; double ab_sq = 0;
     int i = 0;
-    while (a*a + b*b <= 4 && i < n)
+    while (a_sq + b_sq <= 4 && i < n)
     {
-        double copy_a = a;
-        a = a * a - b * b + x;
-        b = 2 * copy_a * b + y;
+        ab_sq = (a + b) * (a + b);
+        a_sq = a * a; 
+        b_sq = b*b;
+        a = a_sq - b_sq + x;
+        b = ab_sq - a_sq- b_sq + y;
         i++;
     }
     return colors[i];
@@ -92,8 +148,11 @@ void scroll(object o, MouseEventArgs e) //zoom in/uit als je scrollt
 }
 void mouse_down_drag(object o, MouseEventArgs e) //bewaart de locatie waar je klikt
 {
-    if (e.Location.Y>52)
+    if (e.Location.Y > 52)
+    {
         mouse_down = e.Location;
+        mouse_down_bool = true;
+    }
 }
 void mouse_up_drag(object o,MouseEventArgs e) //verandert center, gebaseert op het verschil
 { //tussen de locatie waar je klikt en waar je loslaat
@@ -129,6 +188,28 @@ void bereken(object o, EventArgs e)
     }
 
 }
+void verander_kleur(object o, EventArgs e)
+{
+    ColorDialog dlg = new ColorDialog();
+    dlg.ShowDialog();
+    if (o == innercolor)
+    {
+        inrclr = dlg.Color;
+        innercolor.BackColor = inrclr;
+        
+    }
+    else
+    { 
+        outrclr = dlg.Color;
+        outercolor.BackColor = outrclr;
+        
+    }
+    colors = gen_palette(inrclr, outrclr);
+    scherm.Invalidate();
+}
+
+innercolor.Click += verander_kleur;
+outercolor.Click += verander_kleur;
 knop.Click += bereken;
 scherm.MouseWheel += scroll;
 scherm.SizeChanged += verander_grootte;
